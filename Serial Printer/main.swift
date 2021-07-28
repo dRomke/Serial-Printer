38c1a8fc8e8b7384383189169b3ac3ab81d23aab
Romuald <romuald.dufaux@enovates.com>
2021-07-28 23:37:32 +0200

    Working random RFID/noise swiper.

--- Serial Printer/main.swift
+++ Serial Printer/main.swift
@@ -2,10 +2,50 @@
 //  main.swift
 //  Serial Printer
 //
-//  Created by Romuald Dufaux on 28/07/2021.
+//  Created by Romuald Dufaux on 13/07/2021.
 //
 
 import Foundation
 
-print("Hello, World!")
+let availablePorts = ORSSerialPortManager.shared().availablePorts
+let serialPort: ORSSerialPort
+
+// MARK: Connect to TTY
+print("Connecting to \(availablePorts[0].name)")	// Last TTY plugged in
+serialPort = availablePorts[0]
+serialPort.baudRate = 9600
+serialPort.open()
+
+// MARK: Generate random RFID data
+while true {
+	var rfidBytes: Data
+	let randomChoice = Int.random(in: 0...4)
+	
+	switch randomChoice {
+	case 0:
+		// Simulate genuine RFID swipe in 1 out of the 10 cases
+		rfidBytes = Data([0x36, 0x39, 0x42, 0x45, 0x42, 0x41, 0x33, 0x44, 0x0D])
+		
+	case 1:
+		// End of RFID swipe
+		rfidBytes = Data([0x0D])
+		
+	case 2:
+		// Generate random data of very long random length
+		let randomLength = Int.random(in: 1...99)
+		rfidBytes = Data((0..<randomLength).map{ _ in UInt8.random(in: 0..<255) })
+		
+	default:
+		// Generate random data of random length
+		let randomLength = Int.random(in: 1...9)
+		rfidBytes = Data((0..<randomLength).map{ _ in UInt8.random(in: 0..<255) })
+	}
+	
+	// Send it!
+	NSLog("Sending \(rfidBytes as NSData)") // rfidBytes.data(using: String.Encoding.ascii)
+	serialPort.send(rfidBytes)
+	
+	// Sleep for random time (in µs, so sometimes very short!)
+	usleep(useconds_t(Int.random(in: 0..<5000000)))
+}
 
